e1ad71a45e62b66bbc2f6fad2c75e7c2b0085863
Sanket Kapse <sanket8397@gmail.com>
2024-04-23 11:25:03 -0700

    changed public to docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: "/portfolio",
   siteMetadata: {
     title: 'Sanket Kapse',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Software Engineering graduate with 4 years of professional experience as a software developer in the banking domain.',
+    siteUrl: 'https://github.com/sanket8397', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
