477dd4cfa6eb4cc006d9851de79d10f7f55620e3
Sanket Kapse <sanket8397@gmail.com>
2024-04-23 11:00:36 -0700

    Updated contact and readme

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I'm all geared up for the next big adventure in software development.
+        Bring on the challenges, because I'm ready to geek out and make some magic happen!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
